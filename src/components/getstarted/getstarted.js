05c2cd5ba3e91ebc0a23961fba0cac7e4e8a3091
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-01 09:04:47 +0300

    edit navigation

--- src/components/getstarted/getstarted.js
+++ src/components/getstarted/getstarted.js
@@ -5,28 +5,29 @@ import { useNavigate } from 'react-router-dom';
 const Getstarted = () => {
 
   const navigate = useNavigate()
-  
+
 
   const handleGetStartedClick = () => {
-    navigate('/homepage'); 
+    navigate('/home');
   };
+
   return (
     <div className="w-full h-auto montserrat flex flex-col items-center mx-auto bg-[#FDFDFD]">
       <div className="min-w-[384px] max-w-[480px] p-4 flex flex-col gap-6">
-      <div className={getstartedStyle['getstarted-body']}>
-      <h1 className={getstartedStyle['getstarted-title']}>
-        You want<br/> Authentic, here<br/> you go!
-      </h1>
-      <p className={getstartedStyle['getstarted-subtitle']}>
-        Find it here, buy it now!
-      </p>
-      <button className={getstartedStyle['getstarted-btn']} onClick={handleGetStartedClick}>
-        Get Started
-      </button>
-    </div>
+        <div className={getstartedStyle['getstarted-body']}>
+          <h1 className={getstartedStyle['getstarted-title']}>
+            You want<br /> Authentic, here<br /> you go!
+          </h1>
+          <p className={getstartedStyle['getstarted-subtitle']}>
+            Find it here, buy it now!
+          </p>
+          <button className={getstartedStyle['getstarted-btn']} onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
-   
+
   );
 };
 
