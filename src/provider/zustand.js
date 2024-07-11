9b195120fb398cb4aa627dabf2c861e87bd85d7f
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2024-07-11 15:54:54 +0300

    adding-zustand

---
+++ src/provider/zustand.js
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+
+const useCartStore = create((set) => ({
+  cart: [],
+  addToCart: (item) => set((state) => ({ cart: [...state.cart, item] })),
+  removeFromCart: (itemId) => set((state) => ({
+    cart: state.cart.filter((item) => item.id !== itemId),
+  })),
+}));
+
+export default useCartStore ;
