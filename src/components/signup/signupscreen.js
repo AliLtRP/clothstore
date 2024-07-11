ce9757a02c56737ee82e7e0cc9decda78011da55
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2024-07-11 13:36:38 +0300

    update order

--- src/components/signup/signupscreen.js
+++ src/components/signup/signupscreen.js
@@ -96,11 +96,6 @@ const Signupscreen = () => {
           I Already Have an Account <a href='##' className={signupStyle['signin-link']} onClick={() => navigate('/signin')}>Login</a>
         </p>
       </div>
-      {error && (
-          <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-            <span className="font-medium">{error}</span>
-          </div>
-        )}
       <button className={signupStyle['signup-login-btn']} onClick={handleSignUpClick}>Create Account</button>
     </div>
     </div>
