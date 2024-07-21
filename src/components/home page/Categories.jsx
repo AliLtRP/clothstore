c972e7ff57d297f0ae754732461f393bafd10630
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2024-07-21 12:49:40 +0300

    skelton-edit

--- src/components/home page/Categories.jsx
+++ src/components/home page/Categories.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import client from '../../api/axios';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
-
-const Categories = () => {
+const Categories = ({ loading }) => {
     const [data, setData] = useState([]);
 
     const fetchData = async () => {
         await client.get('/category/all')
             .then(res => {
-                const data = res.data.data
+                const data = res.data.data;
                 const sortData = data.sort((a, b) => a.priority < b.priority);
                 setData(sortData);
             })
-            .catch(e => console.log(e))
-    }
+            .catch(e => console.log(e));
+    };
 
     useEffect(() => {
         fetchData();
@@ -22,22 +23,27 @@ const Categories = () => {
 
     return (
         <div className='w-full flex justify-between'>
-            {
-                data.map((v, i) => {
-                    return (
-                        <div className='flex flex-col w-14' key={i}>
-                            <Link to={`/category/product/${v.id}/${v.name}`} >
-                                <div className='w-14 h-14 rounded-full bg-black'>
-                                    <img src={v.img} alt="category images" className='rounded-full h-14 w-14 object-fill' />
-                                </div>
-                            </Link>
-                            <p className='text-center pt-2 text-[#21003D] text-[10px] leading-[16px]'>{v.name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div >
-    )
-}
+            {loading ? (
+                Array.from({ length: 5 }).map((_, i) => (
+                    <div className='flex flex-col w-14' key={i}>
+                        <Skeleton circle={true} height={56} width={56} />
+                        <Skeleton width={56} height={16} className='mt-2' />
+                    </div>
+                ))
+            ) : (
+                data.map((v, i) => (
+                    <div className='flex flex-col w-14' key={i}>
+                        <Link to={`/category/product/${v.id}/${v.name}`} >
+                            <div className='w-14 h-14 rounded-full bg-black'>
+                                <img src={v.img} alt="category images" className='rounded-full h-14 w-14 object-fill' />
+                            </div>
+                        </Link>
+                        <p className='text-center pt-2 text-[#21003D] text-[10px] leading-[16px]'>{v.name}</p>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
 
-export default Categories
+export default Categories;
--- src/components/home page/HomePage.jsx
+++ src/components/home page/HomePage.jsx
@@ -1,74 +1,101 @@
-import Footer from "./Footer"
-import { MicSVG, SearchSVG } from "./icons"
-import { NavBar, Categories, Card, Deal } from "./index"
+import Footer from "./Footer";
+import { MicSVG, SearchSVG } from "./icons";
+import { NavBar, Categories, Card, Deal } from "./index";
 import NewArrival from "./NewArrival";
 import { useEffect, useState } from "react";
 import client from "../../api/axios";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import List from "./List";
 
 const HomePage = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const fetchData = async () => {
-        try {
-            const res = await client.get(`/banner/all`);
-            setData(res.data.data);
-        } catch (e) {
-            console.log(e);
-        } finally {
-            setLoading(false);
-        }
-    };
+  const fetchData = async () => {
+    try {
+      const res = await client.get(`/banner/all`);
+      setData(res.data.data);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-
-    return (
-        <div className="w-full h-auto montserrat flex flex-col items-center mx-auto bg-[#FDFDFD]">
-            <div className="min-w-[24rem] max-w-sm p-4 flex flex-col gap-6">
-                <NavBar />
-                <div className="w-full flex justify-center relative mt-12">
-                    <SearchSVG className="absolute top-2/4 left-6 translate-x-[-50%] translate-y-[-50%]" />
-                    <input type="text" placeholder="Search any Product.." className="bg-white w-full p-2 pl-10 rounded-md box-shadow text-sm text-[#BBBBBB]" />
-                    <MicSVG className="absolute top-2/4 right-2.5 translate-x-[-50%] translate-y-[-50%]" />
-                </div>
-                <div className="w-full h-auto gap-2">
-                    {loading ? <Skeleton height={80} /> : <Categories />}
-                </div>
-
-                <div className="h-full flex flex-col gap-12 mt-2">
-                    {loading
-                        ? <>
-                            <Skeleton height={140} />
-                            <Skeleton height={200} />
-                            <Skeleton height={200} />
-                        </>
-                        : data.map((v, i) => {
-                            if (v.type === 'slider') {
-                                return <Card key={i} banners={v.banners} />;
-                            } else if (v.type === 'banner') {
-                                return <NewArrival key={i} title={v.title} description={v.description} />;
-                            } else if (v.type === 'deal') {
-                                return <Deal key={i} products={v.products_ids} endTime={v.end_date} />;
-                            } else if (v.type === 'list') {
-                                return <List key={i} v={v} />;
-                            } else {
-                                return null;
-                            }
-                        })
-                    }
-                </div>
-            </div>
-            <div className="max-w-sm flex justify-center">
-                <Footer path="home" />
-            </div>
+  return (
+    <div className="w-full h-auto montserrat flex flex-col items-center mx-auto bg-[#FDFDFD]">
+      <div className="min-w-[24rem] max-w-sm p-4 flex flex-col gap-6">
+        <NavBar loading={loading} />
+        <div className="w-full flex justify-center relative mt-12">
+          {loading ? (
+            <Skeleton width={350} height={35} className="rounded-md" />
+          ) : (
+            <>
+              <SearchSVG className="absolute top-2/4 left-6 translate-x-[-50%] translate-y-[-50%]" />
+              <input
+                type="text"
+                placeholder="Search any Product.."
+                className="bg-white w-full p-2 pl-10 rounded-md box-shadow text-sm text-[#BBBBBB]"
+              />
+              <MicSVG className="absolute top-2/4 right-2.5 translate-x-[-50%] translate-y-[-50%]" />
+            </>
+          )}
         </div>
-    );
-}
+        <div className="w-full h-auto gap-2">
+          <Categories loading={loading} />
+        </div>
+
+        <div className="h-full flex flex-col gap-12 mt-2">
+          {
+            loading
+                ? <>
+                    <Skeleton height={140} />
+                    <Skeleton height={200} />
+                    <Skeleton height={200} />
+                </>
+                :
+            data.map((v, i) => {
+              if (v.type === "slider") {
+                return <Card key={i} banners={v.banners} loading={loading} />;
+              } else if (v.type === "banner") {
+                return (
+                  <NewArrival
+                    key={i}
+                    title={v.title}
+                    description={v.description}
+                  />
+                );
+              } else if (v.type === "deal") {
+                return (
+                  <Deal
+                    key={i}
+                    products={v.products_ids}
+                    endTime={v.end_date}
+                  />
+                );
+              } else if (v.type === "list") {
+                return <List key={i} v={v} />;
+              } else {
+                return null;
+              }
+            })
+          }
+        </div>
+      </div>
+      <div className="max-w-sm flex justify-center">
+        {loading ? (
+          <Skeleton height={60} width={350} />
+        ) : (
+          <Footer path="home" />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
