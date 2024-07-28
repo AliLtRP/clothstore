d4b4923be2fa6788afdd924309e83fee3465a08c
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-28 11:46:55 +0300

    edit sign up

--- src/components/btnComp.jsx
+++ src/components/btnComp.jsx
@@ -1,47 +1,27 @@
 import React from "react";
 
-const ButtonComp = ({ title, disabled, className, width, loading, onClick , small , big }) => {
+const styles = {
+  disable: " bg-[#f83758] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] opacity-50 cursor-not-allowed",
+  loading: " bg-[#750417] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] cursor-not-allowed",
+  small: " bg-[#f83758] text-white text-center text-[14px] px-5 py-3 font-bold rounded-[5px] cursor-pointer",
+  big: " bg-[#f83758] text-white text-center text-[20px] px-5 py-3 font-bold rounded-[5px] cursor-pointer",
+  default: " bg-[#f83758] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] cursor-pointer"
+}
+
+const getClassNames = ({ disabled, loading, small, big }) => {
+  if (disabled) return styles.disable;
+  if (loading) return styles.loading;
+  if (small) return styles.small;
+  if (big) return styles.big;
+  return styles.default;
+};
+
+const ButtonComp = ({ title, disabled, className, width, loading, onClick, small, big }) => {
+  const buttonStyle = getClassNames({ disabled, loading, small, big });
+
   return (
     <>
-      {disabled ? (
-        <div
-          className={className + ` bg-[#f83758] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] opacity-50 cursor-not-allowed`}
-          style={{ width: width }}
-        >
-          {title}
-        </div>
-      ) : loading ? (
-        <div
-          className={`bg-[#750417] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] cursor-not-allowed`}
-          style={{ width: width }}
-        >
-          Loading...
-        </div>
-      ) : small == true ? (
-        <div
-        className={`bg-[#f83758] text-white text-center text-[14px] px-5 py-3 font-bold rounded-[5px] cursor-pointer`}
-        style={{ width: width }}
-        onClick={onClick}
-        >
-          {title}
-        </div>
-      ): big == true ? (
-        <div
-        className={`bg-[#f83758] text-white text-center text-[20px] px-5 py-3 font-bold rounded-[5px] cursor-pointer`}
-        style={{ width: width }}
-        onClick={onClick}
-        >
-          {title}
-        </div>
-      ):(
-        <div
-          className={`bg-[#f83758] text-white text-center text-[18px] px-5 py-3 font-bold rounded-[5px] cursor-pointer`}
-          style={{ width: width }}
-          onClick={onClick}
-        >
-          {title}
-        </div>
-      )}
+      <button className={`${className} ${buttonStyle}`} style={{ width }} onClick={onClick}>{loading ? "Loading..." : "Log In"}</button>
     </>
   );
 };
